31115d83f384813fc09f93ae683fa1b7b401bf15
unknown <sijibomiolajubu@gmail.com>
2021-03-17 16:56:20 +0100

    proxy here and dear

--- src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-// import { proxy } from 'http-proxy-middleware';
-
-
-// export const app=(app) =>{
-//   app.use(
-//     proxy(["/api",], { target: "http://django-movies-backend.herokuapp.com" })
-//   );
-// };
